628a9352c63a78f7d771b03a5df3cfc94172d5b0
victorog17 <victor.og17@gmail.com>
2024-09-24 21:39:58 -0300

    Construindo repo e web scraping

---
+++ src/extract_kaggle/extract_kaggle.py
@@ -0,0 +1,33 @@
+import subprocess
+import os
+
+from pathlib import Path
+
+# Configurando os Path
+new_folder_kaggle = r'C:\Users\Victor_1\Documents\VSCode\fiap_tech_challenge_03\src\data\raw'
+download_path = r'C:\Users\Victor_1\Documents\VSCode\fiap_tech_challenge_03\src\data\raw'
+
+# Nome do conjunto de dados
+dataset_name = 'goyaladi/flight-dataset'
+
+file_name = f"{download_path}/{dataset_name.split('/')[1]}.zip"
+file_name = Path(file_name)
+print(f"Esse é o path montado {file_name}")
+
+if not os.path.exists(file_name):
+    # Criando nova pasta para arquivos kaggle
+    subprocess.run(['mkdir', '-p', new_folder_kaggle])
+
+    # Baixando arquivo zip do kaggle
+    subprocess.run('kaggle', 'datasets', 'download', '-d', dataset_name, '-p', str(download_path))
+
+    # Descompactando os arquivos baixados com a opção -o para substituir sem prompt
+    subprocess.run(['tar', '-o', file_name, '-d', download_path])
+
+    # Deletar arquivo zip
+    subprocess.run(['rm', '-r', file_name, '-d', download_path])
+
+#command = ['tar', '-xf', zip_file, '-C', extract_to]
+
+# Executa o comando
+#subprocess.run(command, check=True)
