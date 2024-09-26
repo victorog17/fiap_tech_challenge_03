a7ac033ad6c26809fcf3ba271e21175c1d7872ab
victorog17 <victor.og17@gmail.com>
2024-09-25 23:32:56 -0300

    Atualizacao extract

--- src/extract_kaggle/extract_kaggle.py
+++ src/extract_kaggle/extract_kaggle.py
@@ -1,5 +1,6 @@
 import subprocess
 import os
+import kaggle
 
 from pathlib import Path
 
@@ -14,12 +15,20 @@ file_name = f"{download_path}/{dataset_name.split('/')[1]}.zip"
 file_name = Path(file_name)
 print(f"Esse é o path montado {file_name}")
 
+download_path = Path(download_path)
+print(f"Esse é o path de download {download_path}")
+
+kaggle.api.authenticate()
+
+kaggle.api.dataset_download_files(dataset_name, path=download_path, unzip=True)
+
+'''
 if not os.path.exists(file_name):
     # Criando nova pasta para arquivos kaggle
     subprocess.run(['mkdir', '-p', new_folder_kaggle])
 
     # Baixando arquivo zip do kaggle
-    subprocess.run('kaggle', 'datasets', 'download', '-d', dataset_name, '-p', str(download_path))
+    subprocess.run(['kaggle', 'datasets', 'download', '-d', dataset_name, '-p', download_path])
 
     # Descompactando os arquivos baixados com a opção -o para substituir sem prompt
     subprocess.run(['tar', '-o', file_name, '-d', download_path])
@@ -28,6 +37,6 @@ if not os.path.exists(file_name):
     subprocess.run(['rm', '-r', file_name, '-d', download_path])
 
 #command = ['tar', '-xf', zip_file, '-C', extract_to]
-
+'''
 # Executa o comando
 #subprocess.run(command, check=True)
