48bfe6a4352de1365515d47694c1a1f642b579ff
victorog17 <victor.og17@gmail.com>
2024-09-28 21:37:21 -0300

    Criacao do modelo de classificacao

--- src/extract_kaggle/extract_kaggle.py
+++ src/extract_kaggle/extract_kaggle.py
@@ -9,7 +9,8 @@ new_folder_kaggle = r'C:\Users\Victor_1\Documents\VSCode\fiap_tech_challenge_03\
 download_path = r'C:\Users\Victor_1\Documents\VSCode\fiap_tech_challenge_03\src\data\raw'
 
 # Nome do conjunto de dados
-dataset_name = 'goyaladi/flight-dataset'
+#dataset_name = 'goyaladi/flight-dataset'
+dataset_name = 'fedesoriano/stroke-prediction-dataset'
 
 file_name = f"{download_path}/{dataset_name.split('/')[1]}.zip"
 file_name = Path(file_name)
