29f94d3c307c4f64b4bd67edfd38e2af1e0cd668
victorog17 <victor.og17@gmail.com>
2024-09-30 22:53:03 -0300

    Inclusao de avaliacao do modelo

--- src/extract_kaggle/extract_kaggle.py
+++ src/extract_kaggle/extract_kaggle.py
@@ -1,5 +1,5 @@
-import subprocess
-import os
+#import subprocess
+#import os
 import kaggle
 
 from pathlib import Path
@@ -23,21 +23,4 @@ kaggle.api.authenticate()
 
 kaggle.api.dataset_download_files(dataset_name, path=download_path, unzip=True)
 
-'''
-if not os.path.exists(file_name):
-    # Criando nova pasta para arquivos kaggle
-    subprocess.run(['mkdir', '-p', new_folder_kaggle])
-
-    # Baixando arquivo zip do kaggle
-    subprocess.run(['kaggle', 'datasets', 'download', '-d', dataset_name, '-p', download_path])
-
-    # Descompactando os arquivos baixados com a opção -o para substituir sem prompt
-    subprocess.run(['tar', '-o', file_name, '-d', download_path])
-
-    # Deletar arquivo zip
-    subprocess.run(['rm', '-r', file_name, '-d', download_path])
-
-#command = ['tar', '-xf', zip_file, '-C', extract_to]
-'''
-# Executa o comando
-#subprocess.run(command, check=True)
+# Link Dataset: https://www.kaggle.com/datasets/fedesoriano/stroke-prediction-dataset
